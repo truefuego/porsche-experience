f0bb5d0c2f9215e61de99bc58dfe29bca45dc2d1
truefuego <ankitsharma15092003@gmail.com>
2025-02-24 14:53:03 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App:React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-slate-500 text-5xl">Porsche Experience</h1>
+    </div>
+  )
+}
+
+export default App;
