f01ecd5a1272dc9c4448b873f62232a9c82aa009
truefuego <ankitsharma15092003@gmail.com>
2025-02-27 01:46:59 +0530

    Fix - Side Panel

--- src/components/SidePanelWrapper.tsx
+++ src/components/SidePanelWrapper.tsx
@@ -4,13 +4,16 @@ import { CrossIconThin } from '../assets/icons/icons';
 
 const SidePanelWrapper:React.FC<ISidePanelWrapperProps> = ({ children, isVisible, setIsVisible, classes }) => {
   return (
-    <div className={`fixed top-0 right-0 h-screen bg-amber-50 text-white flex flex-col items-start justify-start transition-all duration-300 ${isVisible ? 'w-[70%]' : 'w-0'} ${classes}`}>
-        <div onClick={() => setIsVisible(false)} className={`p-4 m-4 cursor-pointer transition-all duration-300 delay-100 ${isVisible ? 'block' : 'hidden'}`}>
+    <div className={`fixed top-0 right-0 h-screen text-white flex flex-col items-start justify-end transition-all duration-300 ${isVisible ? ' w-screen' : 'w-0'} ${classes}`}>
+      <div className='absolute w-screen h-screen cursor-pointer' onClick={() => setIsVisible(false)} />
+      <div className={`absolute h-screen transition-all right-0 bg-amber-50 duration-200 z-10 delay-100 ${isVisible ? 'w-screen md:w-[70%]' : 'w-0'}`}>
+        <div onClick={() => setIsVisible(false)} className='cursor-pointer m-8'>
           <CrossIconThin />
         </div>
-        <div className={`transition-all pt-8 pl-8 duration-300 delay-100 ${isVisible ? 'block' : 'hidden'}`}>
+        <div className={`mt-8 ml-8 ${isVisible ? 'block': 'hidden'}`}>
           {children}
         </div>
+      </div>
     </div>
   )
 }
